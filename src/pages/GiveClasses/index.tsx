28241993f33e09a85671bbdda12b435f2392d0c4
Gustavo Benevenuto <gustavo.benesilva@gmail.com>
2020-08-06 18:23:36 -0300

    Conclusão Landing Page

---
+++ src/pages/GiveClasses/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+
+import styles from './styles';
+
+const GiveClasses = () => {
+    return(
+        <View>
+            <Text>
+                A
+            </Text>
+        </View>
+    )
+}
+
+export default GiveClasses;
